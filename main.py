a8685c0767a2e5d6c201c4a10506026336caa7a8
Tomas Hornicek <tomas@Tomass-MBP.attlocal.net>
2024-12-31 16:22:22 -0500

    Path Parameters and Numeric Validations

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def read_items(
-        item_id: Annotated[int, Path(title="The ID of the item to get")],
-        q: Annotated[Union[str, None], Query(alias="item-query")] = None,
+        *,
+        item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+        q: str,
+        size: Annotated[float, Query(gt=0, lt=10.5)],
 ):
     results = {"item_id": item_id}
     if q:
