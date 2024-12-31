a322668faef2ef3559af58c08c65033b15561e20
Tomas Hornicek <tomas@Tomass-MBP.attlocal.net>
2024-12-31 16:16:27 -0500

    Declaring Metadata Completed

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from typing import Annotated
+from typing import Annotated, Union
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
-
-@app.get("/items/")
-async def read_items(q: Annotated[list[str], Query()] = ["foo", "bar"]):
-    query_items = {"q": q}
-    return query_items
+@app.get("/items/{item_id}")
+async def read_items(
+        item_id: Annotated[int, Path(title="The ID of the item to get")],
+        q: Annotated[Union[str, None], Query(alias="item-query")] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q":q})
+    return results
