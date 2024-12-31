45c77c25813c536846b2218dcf3d799912d0e5ff
Tomas Hornicek <tomas@Tomass-MBP.attlocal.net>
2024-12-31 14:19:59 -0500

    Use of Pattern Instead of Regex Completed

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from __future__ import annotations
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Annotated
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price:float
-    tax: float | None = None
+from fastapi import FastAPI, Query
 
-app = FastAPI
+app = FastAPI()
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/items/")
+async def read_items(
+        q: Annotated[
+            str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+        ] = None,
+):
+    results = {"items": [{"item_id":"Foo"}, {"item_id":"Bar"}]}
     if q:
-        result.update({"q":q})
-    return result
+        results.update({"q":q})
+    return results
